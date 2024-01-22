2da2f42610581aa00db84d63430b5deaf077ea87
Yuvraj <rathore1965@outlook.in>
2024-01-22 17:59:49 +0530

    cookie error

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,16 +3,36 @@ const NewUser = require("../models/signupSch");
 
 const auth = async(req,res, next) =>{
     try {
-        const token = req.cookies.jwt;
+        try {
+            const token = req.cookies.jwt;
+            return token;
+        } catch (error) {
+            res.status(400).render("error",{
+                output: `error in first phase : ${error}`
+            })
+        }
+
         try {
             const verifyUser = jwt.verify(token, process.env.SECRET_KEY);
             console.log(verifyUser);
+            return verifyUser;
         } catch (error) {
-            console.error(`error inn verification : ${error}`)
+            res.status(400).render("error",{
+                output: `Error in verifcation : ${error}`
+            })
         }
 
-        const user = await NewUser.findOne({_id:verifyUser.id})
-        console.log(user);
+        try {
+            const user = await NewUser.findOne({_id:verifyUser.id})
+            console.log(user);
+            return user;
+        } catch (error) {
+            res.status(400).render("error",{
+                output: `Error in 3rd phase : ${error}`
+            })
+        }
+
+
 
         req.user = user;
         req.token = token;
